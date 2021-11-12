bcb35e13a4822b12bcd2df85eaf92c6c7ad4bfa4
Craig <craigwilsonmusic@gmail.com>
2021-11-12 18:07:16 +0000

    need to add date package

--- src/components/Date.js
+++ src/components/Date.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
+
 function Date(props) {
 
-
-    const date = {
-        currentDateTime: Date().toLocaleString()
-    }
 
     return (
         <div className="center">
             <h3 className="subheader">Thursday 11th November</h3>
-            {this.state.currentDateTime}
+            
         </div>
     );
 }
